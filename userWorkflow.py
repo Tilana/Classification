c25482a6dc9bbafd7429b0f1acbbb465fe5378f9
tilana <NatalieWidmann@gmx.de>
2017-12-19 20:05:03 +0100

    code clean up demo user workflow with sample sentences

--- userWorkflow.py
+++ userWorkflow.py
@@ -53,43 +53,14 @@ def userWorkflow():
         sentence,category,value = getSentenceSample(sentences, categoryID, sentences_config)
         train(sentence, category, value)
 
-    pdb.set_trace()
-
-    #sentenceClassifier = ClassificationModel(target=sentences_config['TARGET'], labelOfInterest=sentences_config['categoryOfInterest'])
-    #sentenceClassifier.data = sentences
-    #sentenceClassifier.createTarget()
-
-    #sentenceClassifier.setDataConfig(sentences_config)
-    #sentenceClassifier.validation = validation
-
-    #sentenceClassifier.splitDataset(train_size=sentences_train_size, random_state=20)
-
-
-    #if analyze:
-    #    coi = sentenceClassifier.sentences[sentenceClassifier.sentences[sentences_config['TARGET']]==sentences_config['categoryOfInterest']]
-    #    document_lengths = [len(word_tokenize(sentence)) for sentence in coi.text]
-    #    plotter = ImagePlotter(True)
-    #    #figure_path = path=os.path.join(PATH, sentences_config['DATASET'], 'figures', data_config['ID'] + '_evidenceSentences' + '.png')
-    #    bins = range(1,100)
-    #    plotter.plotHistogram(document_lengths, log=False, title= sentences_config['ID'] + ' frequency of evidence sentences length', xlabel='sentence length', ylabel='frequency', bins=bins, path=None)
-    #    print 'max: ' + str(max(document_lengths))
-    #    print 'min 0.5: ' + str(min(document_lengths))
-    #    print 'median: ' + str(np.median(document_lengths))
-
-    #sentenceClassifier.max_document_length = max([len(x.split(" ")) for x in sentenceClassifier.trainData.text])
-    #print 'Maximal sentence length ' + str(sentenceClassifier.max_document_length)
-
-
-    cnnClassification(sentenceClassifier, ITERATIONS=3, BATCH_SIZE=64, filter_sizes=[2,3,4,5])
-
-
-    print 'Split Validation Data In Sentences'
+    # Get Full text documents
     data = pd.read_pickle(sentences_config['full_doc_path'])
-    validationIndices = sentenceClassifier.validationData.docID.unique()
-    data = data[data.id.isin(validationIndices)]
 
-    doc = data.sample(1, random_state=42).iloc[0]
-    evidenceSentences = predictDoc(doc, categoryID)
+    # Predict label of sentences in documents
+    for numberSample in xrange(5):
+        sample = data.sample(1, random_state=42).iloc[0]
+        evidenceSentences = predictDoc(sample[['text', 'title']], categoryID)
+        print evidenceSentences
 
     pdb.set_trace()
 
