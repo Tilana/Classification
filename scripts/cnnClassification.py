6475121a2812cff1aea683d1d44f7293d1bc22c6
tilana <NatalieWidmann@gmx.de>
2018-01-08 21:10:17 +0100

    make saving the model and the results optional

--- scripts/cnnClassification.py
+++ scripts/cnnClassification.py
@@ -8,7 +8,7 @@ import gensim.models.keyedvectors as w2v_model
 from lda import Viewer, NeuralNet, Evaluation, data_helpers
 
 
-def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filter_sizes=[3,4,5], pretrainedWordEmbeddings=True):
+def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filter_sizes=[3,4,5], pretrainedWordEmbeddings=True, storeModel=1):
 
     np.random.seed(42)
 
@@ -28,7 +28,8 @@ def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filte
         Y_validation = pd.get_dummies(model.validationTarget.tolist()).as_matrix()
 
     vocabulary = vocab_processor.vocabulary_
-    vocab_processor.save(model.output_dir + 'preprocessor')
+    if storeModel:
+        vocab_processor.save(model.output_dir + 'preprocessor')
 
     #pdb.set_trace()
 
@@ -43,8 +44,8 @@ def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filte
     sess = tf.Session(config=session_config)
 
     with sess.as_default():
-
-        nn.setSummaryWriter(model.output_dir, tf.get_default_graph())
+        if storeModel:
+            nn.setSummaryWriter(model.output_dir, tf.get_default_graph())
         nn.buildNeuralNet(cnnType, sequence_length=model.max_document_length, vocab_size=len(vocabulary), optimizerType='Adam', filter_sizes=filter_sizes)
 
         sess.run(tf.global_variables_initializer())
@@ -59,7 +60,8 @@ def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filte
                 initW[idx] = word2vec.word_vec(word)
             sess.run(nn.W.assign(initW))
 
-        nn.setSaver()
+        if storeModel:
+            nn.setSaver()
 
         batches = data_helpers.batch_iter(list(zip(X_train, Y_train)), BATCH_SIZE, ITERATIONS, shuffle=False)
 
@@ -81,8 +83,9 @@ def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filte
             evaluation = Evaluation(np.argmax(y_batch,1), predLabels)
             evaluation.computeMeasures()
 
-            nn.writeSummary(train_summary, c)
-            nn.writeSummary(grad_summary, c)
+            if storeModel:
+                nn.writeSummary(train_summary, c)
+                nn.writeSummary(grad_summary, c)
 
             print('Train step:')
             print('{}: step {}, loss {:g}, acc {:g}, precision {:g}, recall {:g}'.format(datetime.now().isoformat(), c, entropy, acc, evaluation.precision, evaluation.recall))
@@ -91,7 +94,8 @@ def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filte
                 testData = {nn.X: X_test, nn.Y_: Y_test, nn.learning_rate: 0, nn.pkeep:1.0}
                 predLabels, test_summary = sess.run([nn.Y, nn.summary], feed_dict=testData)
 
-                nn.writeSummary(test_summary, c, 'test')
+                if storeModel:
+                    nn.writeSummary(test_summary, c, 'test')
 
                 evaluation = Evaluation(np.argmax(Y_test,1), predLabels)
                 evaluation.computeMeasures()
@@ -104,7 +108,8 @@ def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filte
         predLabels, entropy, accuracy = sess.run([nn.Y, nn.cross_entropy, nn.accuracy], feed_dict=testData)
         evaluation = Evaluation(np.argmax(Y_test,1), predLabels)
         evaluation.computeMeasures()
-        nn.saveCheckpoint(sess, model.output_dir + 'checkpoints/model', c)
+        if storeModel:
+            nn.saveCheckpoint(sess, model.output_dir + 'checkpoints/model', c)
 
         print('Evaluation 2:')
         print('{}: step {}, entropy {:}, acc {:g}, precision {:g}, recall {:g}'.format(datetime.now().isoformat(), c, entropy, accuracy, evaluation.precision, evaluation.recall))
@@ -116,8 +121,9 @@ def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filte
         model.evaluation.confusionMatrix()
         model.classifierType = 'CNN'
 
-        viewer = Viewer(model.name, prefix='..')
-        viewer.classificationResults(model, name= nrTrainData + '_test', normalized=False, docPath=model.doc_path)
+        if storeModel:
+            viewer = Viewer(model.name, prefix='..')
+            viewer.classificationResults(model, name= nrTrainData + '_test', normalized=False, docPath=model.doc_path)
 
         ## Validation Data
         if model.validation:
@@ -129,11 +135,13 @@ def cnnClassification(model, cnnType='cnn', BATCH_SIZE=64, ITERATIONS=100, filte
             model.evaluate(subset='validation')
             model.evaluation.confusionMatrix()
 
-
-            viewer = Viewer(model.name, prefix='..')
-            viewer.classificationResults(model, name= nrTrainData + '_validation', subset='validation', normalized=False, docPath=model.doc_path)
+            if storeModel:
+                viewer = Viewer(model.name, prefix='..')
+                viewer.classificationResults(model, name= nrTrainData + '_validation', subset='validation', normalized=False, docPath=model.doc_path)
 
         sess.close()
         tf.reset_default_graph()
+
+        return model
 
 
