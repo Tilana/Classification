6e812fb15e6a8ef91f10e2a61891588e8dfe255a
tilana <NatalieWidmann@gmx.de>
2018-01-24 10:44:56 +0100

    use pre-trained vocabulary for standard classification

--- scripts/classificationScript.py
+++ scripts/classificationScript.py
@@ -5,12 +5,16 @@ from lda.docLoader import loadTargets, loadConfigFile
 from lda import Preprocessor, ClassificationModel, Viewer
 import pandas as pd
 
+VOCABULARY_PATH = 'vocabulary.txt'
+classifier = 'BernoulliNB'
 
 def classificationScript():
 
     configFile = '../dataConfig.json'
     data_config_name = 'ICAAD_DV_sentences'
-    data_config_name = 'ICAAD_SA_sentences'
+    #data_config_name = 'ICAAD_SA_sentences'
+
+    vocabulary = pd.read_pickle(VOCABULARY_PATH)
 
     data_config = loadConfigFile(configFile, data_config_name)
     data = pd.read_csv('../' + data_config['data_path'], encoding ='utf8')
@@ -19,24 +23,20 @@ def classificationScript():
     balanceData = 1
     validation = 1
     preprocessing = 1
-    train_size = 60
-
-    random_state = 20
-
+    train_size = 40
 
     features = ['tfidf']
 
     if balanceData:
         posSample = data[data[data_config['TARGET']]==data_config['categoryOfInterest']]
-        negSample = data[data[data_config['TARGET']] == data_config['negCategory']].sample(len(posSample), random_state=random_state)
+        negSample = data[data[data_config['TARGET']] == data_config['negCategory']].sample(len(posSample))
         data = pd.concat([posSample, negSample])
 
     if preprocessing:
-        preprocessor = Preprocessor()
+        preprocessor = Preprocessor(vocabulary=vocabulary)
         data.text = data.text.apply(preprocessor.cleanText)
         texts = data.text.tolist()
         data['tfidf'] = preprocessor.trainVectorizer(texts)
-
 
     model = ClassificationModel(target=data_config['TARGET'], labelOfInterest=data_config['categoryOfInterest'])
     model.data = data
@@ -45,7 +45,7 @@ def classificationScript():
     model.setDataConfig(data_config)
     model.validation = validation
 
-    model.splitDataset(train_size=train_size, random_state=random_state)
+    model.splitDataset(train_size=train_size)
 
     nrTrainData = str(len(model.trainData))
 
@@ -55,16 +55,13 @@ def classificationScript():
         collection.correlation =  analyser.correlateVariables(collection)
 
 
-    model.buildClassifier('LogisticRegression', params={'random_state':random_state})
     model.whitelist = None
-
+    (score, params) = model.gridSearch(features) #, scoring=weightedFscore, scaling=False, pca=pca, components=pcaComponents)
+    model.buildClassifier(classifier, params=params)
     model.trainClassifier(features)
 
-    #(score, params) = model.gridSearch(features) #, scoring=weightedFscore, scaling=False, pca=pca, components=pcaComponents)
-    #print('Best score: %0.3f' % score)
     model.predict(features)
     model.evaluate()
-
 
     try:
         model.evaluation.confusionMatrix(model.targetLabels)
