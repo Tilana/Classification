41090b356a5c7f20bcfc70671c10bb8acbca15db
tilana <NatalieWidmann@gmx.de>
2018-02-02 14:33:12 +0100

    get Out-Of-Vocabulary words

--- scripts/getPretrainedEmbedding.py
+++ scripts/getPretrainedEmbedding.py
@@ -8,6 +8,7 @@ def getPretrainedEmbedding(vocab):
             word2vec = w2v_model.KeyedVectors.load_word2vec_format(PATH, binary=True)
             vocabulary = {key:value.index for key, value in word2vec.vocab.iteritems() if key.islower() and '_' not in key}
             vocabIntersection = list(set(vocab.keys()).intersection(vocabulary.keys()))
+            wordsNotInWord2vec = list(set(vocab.keys()).difference(vocabIntersection))
             initW = np.random.uniform(-0.25, 0.25, (len(vocab), 300))
             for word in vocabIntersection:
                 idx = vocab.get(word)
