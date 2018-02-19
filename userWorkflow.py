df481cf354c9c0ca3df6f9b40f484842eba5dfaf
tilana <NatalieWidmann@gmx.de>
2018-02-19 09:18:53 +0100

    add retraining option

--- userWorkflow.py
+++ userWorkflow.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from lda.docLoader import loadConfigFile
+import tensorflow as tf
+import os
+import argparse as _argparse
 from predictDoc import predictDoc
 from train import train
+from shutil import rmtree
 
 def getSentenceSample(sentences, categoryID, sentences_config):
     sentence = sentences.sample(1).iloc[0]
@@ -29,11 +33,23 @@ def userWorkflow():
 
     # Get Full text documents
     data = pd.read_pickle(sentences_config['full_doc_path'])
-    for numberSample in xrange(10):
+    for numberSample in xrange(3):
         sample = data.sample(1).iloc[0]
         evidenceSentences = predictDoc(sample[['text', 'title']], categoryID)
 
-        print evidenceSentences
+        #print evidenceSentences
+
+    # Remove model
+    rmtree(os.path.join('runs', categoryID), ignore_errors=True)
+    #tf.app.flags._global_parser = _argparse.ArgumentParser()
+
+    print 'RETRAIN'
+
+    for numberSample in xrange(10):
+        sentence,category,value = getSentenceSample(sentences, categoryID, sentences_config)
+        train(sentence, category, value)
+        #train(evidence['evidence']['text'], value + property, evidence['isEvidence'])
+
 
 
 if __name__=='__main__':
