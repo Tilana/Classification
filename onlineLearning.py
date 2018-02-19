b30ce27267c588931cad6a42a65a73a90c73204f
tilana <NatalieWidmann@gmx.de>
2018-02-19 09:23:21 +0100

    only add test data value if evidenceSentences are found

--- onlineLearning.py
+++ onlineLearning.py
@@ -23,7 +23,7 @@ def onlineLearning():
     classifier.data = sentences
     classifier.createTarget()
 
-    classifier.splitDataset(train_size=0.9, random_state=7)
+    classifier.splitDataset(train_size=0.97, random_state=7)
 
     # Train Classifier
     for numberSample in xrange(20):
@@ -32,14 +32,15 @@ def onlineLearning():
 
     # Predict label of sentences in documents
     for ind, sample in classifier.testData.iterrows():
+        #pdb.set_trace()
         try:
             evidenceSentences = predictDoc(sample, categoryID)
+            if len(evidenceSentences)>=1:
+                classifier.testData.loc[ind, 'predLabel'] = 1
+                classifier.testData.loc[ind, 'probability'] = evidenceSentences.probability.tolist()[0]
         except:
-            print 'WARNING: PredictDoc.py of sample ' + str(sample.text) + ' was not successful'
+            print 'WARNING: PredictDoc.py of sample "' + str(sample.text) + '" was not successful'
 
-        if len(evidenceSentences)>=1:
-            classifier.testData.loc[ind, 'predLabel'] = 1
-            classifier.testData.loc[ind, 'probability'] = evidenceSentences.probability.tolist()[0]
 
     classifier.testData.predLabel.fillna(0, inplace=True)
 
